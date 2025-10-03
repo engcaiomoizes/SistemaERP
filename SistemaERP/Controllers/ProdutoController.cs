25860cdf54f13854849ec1313546a9a915d0cd3b
Caio Moizes Santos <engcaiomoizes@gmail.com>
2025-10-03 17:09:15 -0300

    Página Produtos criada

---
+++ SistemaERP/Controllers/ProdutoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaERP.Models;
+using System.Diagnostics;
+
+namespace SistemaERP.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly ILogger<ProdutoController> _logger;
+
+        public ProdutoController(ILogger<ProdutoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
